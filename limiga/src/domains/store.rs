8293ce1173f0533eff708a6692cdb94afe109f16
Maarten Flippo <maartenflippo@outlook.com>
2023-05-10 21:58:13 +0200

    Add global domain IDs, and a test to check the operation of the store.
    Global domain IDs are unique between domain types. This is necessary for
    propagators to be linked to variables, independently of the generic
    parameters that govern the domain and type of the variable.

--- limiga/src/domains/store.rs
+++ limiga/src/domains/store.rs
@@ -5,12 +5,17 @@ use super::BitSetDomain;
 /// A handle to a domain.
 pub struct DomainId<Dom> {
     index: usize,
+    global_id: GlobalDomainId,
     dom: PhantomData<Dom>,
 }
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct GlobalDomainId(usize);
 
 #[derive(Default)]
 pub struct Domains {
     bitsets: Vec<BitSetDomain>,
+    next_global_id: usize,
 }
 
 /// A domain store is a container of domains. Once domains have been allocated to the store, they
@@ -30,8 +35,11 @@ impl DomainStore<BitSetDomain> for Domains {
     fn alloc(&mut self, domain: BitSetDomain) -> DomainId<BitSetDomain> {
         self.bitsets.push(domain);
 
+        self.next_global_id += 1;
+
         DomainId {
-            index: self.bitsets.len(),
+            index: self.bitsets.len() - 1,
+            global_id: GlobalDomainId(self.next_global_id),
             dom: PhantomData,
         }
     }
@@ -45,13 +53,45 @@ impl DomainStore<BitSetDomain> for Domains {
     }
 }
 
+impl<Dom> DomainId<Dom> {
+    pub fn global_id(&self) -> GlobalDomainId {
+        self.global_id
+    }
+}
+
 impl<Dom> Clone for DomainId<Dom> {
     fn clone(&self) -> Self {
         DomainId {
             index: self.index,
+            global_id: self.global_id,
             dom: PhantomData,
         }
     }
 }
 
 impl<Dom> Copy for DomainId<Dom> {}
+
+#[cfg(test)]
+mod tests {
+    use crate::domains::Domain;
+
+    use super::*;
+
+    #[test]
+    fn domain_id_returns_appropriate_domain_reference() {
+        let mut store = Domains::default();
+
+        let d1 = store.alloc(BitSetDomain::new(1, 10));
+        let d2 = store.alloc(BitSetDomain::new(5, 50));
+        assert_ne!(d1.global_id(), d2.global_id());
+
+        let d1 = store.read(d1);
+        let d2 = store.read(d2);
+
+        assert_eq!(1, d1.min());
+        assert_eq!(10, d1.max());
+
+        assert_eq!(5, d2.min());
+        assert_eq!(50, d2.max());
+    }
+}

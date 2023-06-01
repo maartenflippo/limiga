5ffbea778ba1b2e995a5e64e2eae3925aef44896
Maarten Flippo <maartenflippo@outlook.com>
2023-06-01 16:44:18 +0200

    Define the interface for the search procedure.

---
+++ limiga/src/search.rs
@@ -0,0 +1,23 @@
+/// A partition of the domain of the CSP. Created by the [`Brancher`] during search.
+pub type Branch<Store> = Box<dyn FnOnce(&mut Store)>;
+
+/// A brancher creates the nodes in the search tree.
+pub trait Brancher<Store> {
+    type Branches: AsRef<[Branch<Store>]>;
+
+    /// Create the new branches of the search tree. If all variables are fixed, and search is
+    /// therefore completed, this should return [`None`].
+    fn branch(&mut self, store: &Store) -> Option<Self::Branches>;
+}
+
+impl<Func, Branches, Store> Brancher<Store> for Func
+where
+    Branches: AsRef<[Branch<Store>]>,
+    Func: Fn(&Store) -> Option<Branches>,
+{
+    type Branches = Branches;
+
+    fn branch(&mut self, store: &Store) -> Option<Self::Branches> {
+        self(store)
+    }
+}
